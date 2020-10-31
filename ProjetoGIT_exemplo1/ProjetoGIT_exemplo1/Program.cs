92c7933884be482ab7c75a2068675deac6bfb695
b13L Lacerda <escanorbateemtodos@outlook.com>
2020-10-30 22:18:15 -0300

    1 commit

---
+++ ProjetoGIT_exemplo1/ProjetoGIT_exemplo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoGIT_exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1° teste VSCode com GIT");
+            Console.ReadKey();
+        }
+    }
+}
